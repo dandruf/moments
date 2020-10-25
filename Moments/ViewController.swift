e6ffcf41fd7102b56ffbeea9f0aff736c50668e3
dandruf <54970918+dandruf@users.noreply.github.com>
2020-10-25 11:17:50 -0400

    Initial Commit

---
+++ Moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by Jasmyne Roberts on 10/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
