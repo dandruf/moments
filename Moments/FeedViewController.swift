f459a2ce982059ebe7b4034db713128eb3f55eeb
dandruf <54970918+dandruf@users.noreply.github.com>
2020-10-25 22:22:10 -0400

    add: bonus loading features

--- Moments/FeedViewController.swift
+++ Moments/FeedViewController.swift
@@ -12,29 +12,79 @@ import AlamofireImage
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var numOfPosts: Int!
     
     var posts = [PFObject]()
+    let customRefreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        loadPosts()
+        
+        customRefreshControl.addTarget(self, action: #selector(loadPosts), for: .valueChanged)
+        tableView.refreshControl = customRefreshControl
 
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        loadPosts()
+        
+//        let query = PFQuery(className: "Posts")
+//        query.includeKey("author")
+//        query.limit = numOfPosts
+//
+//        query.findObjectsInBackground { (posts, error) in
+//            if posts != nil {
+//                self.posts = posts!
+//                self.tableView.reloadData()
+//            }
+//        }
+        
+    }
+    
+    @objc func loadPosts() {
+        
+        numOfPosts = 20
         
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
-        query.limit = 20
+        query.limit = numOfPosts
         
         query.findObjectsInBackground { (posts, error) in
             if posts != nil {
                 self.posts = posts!
                 self.tableView.reloadData()
+                self.customRefreshControl.endRefreshing()
+            } else {
+                print("Could not load posts")
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == posts.count {
+            loadMorePosts()
+        }
+    }
+    
+    func loadMorePosts() {
+        numOfPosts = numOfPosts + 20
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numOfPosts
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            } else {
+                print("Error: \(error ?? "could not load more posts")")
             }
         }
     }
@@ -42,6 +92,12 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
     }
+    
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        if indexPath.row + 1 == posts.count {
+//            loadMorePosts()
+//        }
+//    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell") as! FeedCell
